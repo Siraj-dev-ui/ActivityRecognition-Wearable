1955ab0544f6d9f89031b261970097c2a9b88c88
Siraj-dev-ui <sirajalig86@gmail.com>
2025-11-18 18:12:44 +0200

    basic code

---
+++ src/basicCode.cpp
@@ -0,0 +1,15 @@
+// #include <Arduino.h>
+// #include <Wire.h>
+
+// void setup()
+// {
+//     pinMode(LED_BUILTIN, OUTPUT); // Set the LED pin as output
+// }
+
+// void loop()
+// {
+//     digitalWrite(LED_BUILTIN, HIGH); // Turn LED on
+//     delay(500);                      // Wait 0.5 sec
+//     digitalWrite(LED_BUILTIN, LOW);  // Turn LED off
+//     delay(500);                      // Wait 0.5 sec
+// }
